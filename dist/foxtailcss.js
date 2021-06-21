5a12f73bcfff628b84314c3068792d0c0ed6025a
blissland <blissland.house@googlemail.com>
2021-06-21 16:29:26 +0100

    mutation observer

--- dist/foxtailcss.js
+++ dist/foxtailcss.js
@@ -1002,68 +1002,90 @@
 
   var Rules = {};
 
-  var addClass = (c) => {
-    Rules[c];
-    if (!(c in Rules)) {
-      Rules[c] = compile(c);
-    }
-  };
-
   var insertBaseStyles = () => {
     var style = document.createElement("style");
     document.head.append(style);
     style.textContent = `
-@keyframes spin { from { transform: rotate(0deg); } to { transform: rotate(360deg); } }
-@keyframes ping { 75%, 100% { transform: scale(2); opacity: 0; } }
-@keyframes pulse { 0%, 100% { opacity: 1; } 50% { opacity: .5; } }
-@keyframes bounce { 0%, 100% { transform: translateY(-25%); animationTimingFunction: cubic-bezier(0.8, 0, 1, 1); }
+  @keyframes spin { from { transform: rotate(0deg); } to { transform: rotate(360deg); } }
+  @keyframes ping { 75%, 100% { transform: scale(2); opacity: 0; } }
+  @keyframes pulse { 0%, 100% { opacity: 1; } 50% { opacity: .5; } }
+  @keyframes bounce { 0%, 100% { transform: translateY(-25%); animationTimingFunction: cubic-bezier(0.8, 0, 1, 1); }
   50% { transform: translateY(0); animationTimingFunction: cubic-bezier(0, 0, 0.2, 1); } }
 
-*, ::before, ::after {
---tw-translate-x: 0;
---tw-translate-y: 0;
---tw-rotate: 0;
---tw-skew-x: 0;
---tw-skew-y: 0;
---tw-scale-x: 1;
---tw-scale-y: 1;
---tw-transform: translateX(var(--tw-translate-x)) translateY(var(--tw-translate-y)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
---tw-border-opacity: 1;
-border-color: rgba(229, 231, 235, var(--tw-border-opacity));
---tw-shadow: 0 0 #0000;
---tw-ring-inset: var(--tw-empty, );
---tw-ring-offset-width: 0px;
---tw-ring-offset-color: #fff;
---tw-ring-color: rgba(59, 130, 246, 0.5);
---tw-ring-offset-shadow: 0 0 #0000;
---tw-ring-shadow: 0 0 #0000;
---tw-blur: var(--tw-empty, );
---tw-brightness: var(--tw-empty, );
---tw-contrast: var(--tw-empty, );
---tw-grayscale: var(--tw-empty, );
---tw-hue-rotate: var(--tw-empty, );
---tw-invert: var(--tw-empty, );
---tw-saturate: var(--tw-empty, );
---tw-sepia: var(--tw-empty, );
---tw-drop-shadow: var(--tw-empty, );
---tw-filter: var(--tw-blur) var(--tw-brightness) var(--tw-contrast) var(--tw-grayscale) var(--tw-hue-rotate) var(--tw-invert) var(--tw-saturate) var(--tw-sepia) var(--tw-drop-shadow);
---tw-backdrop-blur: var(--tw-empty, );
---tw-backdrop-brightness: var(--tw-empty, );
---tw-backdrop-contrast: var(--tw-empty, );
---tw-backdrop-grayscale: var(--tw-empty, );
---tw-backdrop-hue-rotate: var(--tw-empty, );
---tw-backdrop-invert: var(--tw-empty, );
---tw-backdrop-opacity: var(--tw-empty, );
---tw-backdrop-saturate: var(--tw-empty, );
---tw-backdrop-sepia: var(--tw-empty, );
---tw-backdrop-filter: var(--tw-backdrop-blur) var(--tw-backdrop-brightness) var(--tw-backdrop-contrast) var(--tw-backdrop-grayscale) var(--tw-backdrop-hue-rotate) var(--tw-backdrop-invert) var(--tw-backdrop-opacity) var(--tw-backdrop-saturate) var(--tw-backdrop-sepia);
-}`;
+  *, ::before, ::after {
+    --tw-translate-x: 0;
+    --tw-translate-y: 0;
+    --tw-rotate: 0;
+    --tw-skew-x: 0;
+    --tw-skew-y: 0;
+    --tw-scale-x: 1;
+    --tw-scale-y: 1;
+    --tw-transform: translateX(var(--tw-translate-x)) translateY(var(--tw-translate-y)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
+    --tw-border-opacity: 1;
+    border-color: rgba(229, 231, 235, var(--tw-border-opacity));
+    --tw-shadow: 0 0 #0000;
+    --tw-ring-inset: var(--tw-empty, );
+    --tw-ring-offset-width: 0px;
+    --tw-ring-offset-color: #fff;
+    --tw-ring-color: rgba(59, 130, 246, 0.5);
+    --tw-ring-offset-shadow: 0 0 #0000;
+    --tw-ring-shadow: 0 0 #0000;
+    --tw-blur: var(--tw-empty, );
+    --tw-brightness: var(--tw-empty, );
+    --tw-contrast: var(--tw-empty, );
+    --tw-grayscale: var(--tw-empty, );
+    --tw-hue-rotate: var(--tw-empty, );
+    --tw-invert: var(--tw-empty, );
+    --tw-saturate: var(--tw-empty, );
+    --tw-sepia: var(--tw-empty, );
+    --tw-drop-shadow: var(--tw-empty, );
+    --tw-filter: var(--tw-blur) var(--tw-brightness) var(--tw-contrast) var(--tw-grayscale) var(--tw-hue-rotate) var(--tw-invert) var(--tw-saturate) var(--tw-sepia) var(--tw-drop-shadow);
+    --tw-backdrop-blur: var(--tw-empty, );
+    --tw-backdrop-brightness: var(--tw-empty, );
+    --tw-backdrop-contrast: var(--tw-empty, );
+    --tw-backdrop-grayscale: var(--tw-empty, );
+    --tw-backdrop-hue-rotate: var(--tw-empty, );
+    --tw-backdrop-invert: var(--tw-empty, );
+    --tw-backdrop-opacity: var(--tw-empty, );
+    --tw-backdrop-saturate: var(--tw-empty, );
+    --tw-backdrop-sepia: var(--tw-empty, );
+    --tw-backdrop-filter: var(--tw-backdrop-blur) var(--tw-backdrop-brightness) var(--tw-backdrop-contrast) var(--tw-backdrop-grayscale) var(--tw-backdrop-hue-rotate) var(--tw-backdrop-invert) var(--tw-backdrop-opacity) var(--tw-backdrop-saturate) var(--tw-backdrop-sepia);
+  }`;
   };
 
+  var update = false;
+
+  var addClass = (c) => {
+    Rules[c];
+    if (!(c in Rules)) {
+      var node = compile(c);
+      if (node) update = true;
+        Rules[c] = node;
+    }
+  };
+
+  const STYLE_ID = "fx-styles";
+
   var insertStyles = () => {
-    var style = document.createElement("style");
-    document.head.append(style);
+    var style = document.getElementById(STYLE_ID);
+
+    if (style === null) {
+      style = document.createElement("style");
+      style.id = STYLE_ID;
+      document.head.append(style);
+    }
     style.textContent = printRules(Object.values(Rules));
+    update = false;
+    console.log("styles updated");
+  };
+
+  var addElement = (el) => {
+    var cl = el.classList;
+    var i = 0, len = cl.length;
+    while (i < len) {
+      addClass(cl[i]);
+      i++;
+    }
   };
 
   var compilePage = () => {
@@ -1073,16 +1095,9 @@ border-color: rgba(229, 231, 235, var(--tw-border-opacity));
 
     console.log(`Found ${els.length} nodes`);
 
-    var total = 0;
     var i = 0, len = els.length;
     while (i < len) {
-      var cl = els[i].classList;
-      var j = 0, len2 = cl.length;
-      while (j < len2) {
-        addClass(cl[j]);
-        j++;
-        total++;
-      }
+      addElement(els[i]);
       i++;
     }
 
@@ -1096,19 +1111,62 @@ border-color: rgba(229, 231, 235, var(--tw-border-opacity));
       i++;
     }
 
-    console.log(`Processed ${total} classes`);
-
     console.log(`Unique ${Object.keys(Rules).length}`);
 
     console.log(`milliseconds elapsed = ${Date.now() - start}`);
   };
 
+  const mutationConfig = {
+    attributes: true,
+    attributeFilter: [ "class" ],
+    childList: true,
+    subtree: true,
+  };
+
+  var start = () => {
+    compilePage();
+
+    const callback = function(mutationsList, observer) {
+      // Use traditional 'for loops' for IE 11
+      for(const mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+
+          for (var i=0; i<mutation.addedNodes.length; i++) {
+            var node = mutation.addedNodes[i];
+            if ((node instanceof HTMLElement) && node.classList) {
+              addElement(node);
+
+              var els = node.querySelectorAll('[class]');
+              for (var j=0; j<els.length; j++) {
+                addElement(els[j]);
+              }
+            }
+          }
+          console.log('A child node has been added or removed.');
+        }
+        else if (mutation.type === 'attributes') {
+          if (mutation.target && (mutation.target instanceof HTMLElement)) {
+            addElement(mutation.target);
+          }
+          console.log('The ' + mutation.attributeName + ' attribute was modified.');
+        }
+      }
+      if (update) insertStyles();
+    };
+
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+
+    // Start observing the target node for configured mutations
+    observer.observe(document.documentElement, mutationConfig);
+  };
+
 
   if (document.readyState === "loading") {
-    window.addEventListener('DOMContentLoaded', compilePage);
+    window.addEventListener('DOMContentLoaded', start);
   }
   else {
-    compilePage();
+    start();
   }
 
 }());
